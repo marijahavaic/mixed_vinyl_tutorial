97b710e2088a3dbe34b6a5ed81f2343538572988
Marija Havaic <marija.havaic@gmail.com>
2023-07-20 18:03:39 +0200

    Use Encore to fetch api and play music

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+import "./bootstrap.js";
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,4 +7,6 @@ import './bootstrap.js';
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import "./styles/app.css";
+
+console.log("Hi!");
